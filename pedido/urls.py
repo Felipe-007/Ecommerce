7962adf1e971ae4ac9e210c0720143eeff455c2a
Felipe <felipe.presidencia@gmail.com>
2021-10-29 09:11:52 -0300

    C10 - Renderizando o carrinho

--- pedido/urls.py
+++ pedido/urls.py
@@ -6,6 +6,6 @@ app_name = 'pedido'
 
 urlpatterns = [
     path('', views.Pagar.as_view(), name='pagar'),
-    path('fecharpedido/', views.FecharPedido.as_view(), name='fecharPedido'),
+    path('salvarpedido/', views.SalvarPedido.as_view(), name='salvarpedido'),
     path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
 ]
